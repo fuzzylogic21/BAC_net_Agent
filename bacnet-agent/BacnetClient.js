da23613a68af452d7abb48e15b8ae7280d982eb2
fuzzylogic21 <77990041+fuzzylogic21@users.noreply.github.com>
2021-01-28 21:39:06 +0530

    Update BacnetClient.js

--- bacnet-agent/BacnetClient.js
+++ bacnet-agent/BacnetClient.js
@@ -1,14 +1,14 @@
 let bacnet = require('node-bacnet');
 
 class BacnetClient {
-    static receiver = '192.168.201.90';
+    static receiver = '192.168.1.90';
     static client = undefined;
     static initialize() {
         if (BacnetClient.client == undefined) {
             this.client = new bacnet({
                 port: 47808,
                 interface: '0.0.0.0',
-                broadcastAddress: '192.168.201.255'
+                broadcastAddress: '192.168.1.255'
             });
         }
     }
